40c7c60f379019e835ee35633ec409d39bf3cf0f
IsaacNZS <jas794613@gmail.com>
2026-05-06 16:14:53 +0630

    fix again3 commit

--- sever/app.js
+++ sever/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173https://isaac-zls-blog.onrender.com",
   }),
 );
 app.use("/user", router);
