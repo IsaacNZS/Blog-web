fd7c949113a7ff169669630e435c71c9257ffe46
IsaacNZS <jas794613@gmail.com>
2026-05-08 23:32:55 +0630

    fix again13 commit

--- sever/app.js
+++ sever/app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "https://isaac-zls-blog.onrender.com",
+    origin: "http://localhost:5173",
   }),
 );
 
@@ -32,7 +32,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://isaac-zls-blog.onrender.com",
+    origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST"],
   },
